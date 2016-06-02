67a38abd659e6a7f70a3c46c2f3af751d1a6460c
Sunho Kim <sunho207@gmail.com>
2016-06-02 18:26:20 -0400

    Login Page

---
+++ app/controllers/Application.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import play.mvc.*;
+
+import views.html.*;
+
+/**
+ * This controller contains an action to handle HTTP requests
+ * to the application.
+ */
+public class Application extends Controller {
+
+    /**
+     * An action that renders an HTML page with a welcome message.
+     * The configuration in the <code>routes</code> file means that
+     * this method will be called when the application receives a
+     * <code>GET</code> request with a path of <code>/</code>.
+     */
+    public Result index() {
+        return ok(index.render());
+    }
+
+    public Result login() {
+        return ok(login.render());
+    }
+
+}
