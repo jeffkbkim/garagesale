f4d3fbdd5c204e4264e2cfa1c9f754074c19c34f
Sunho Kim <sunho207@gmail.com>
2016-07-24 23:53:03 -0400

    Tests

--- test/ModelsTest.java
+++ test/ModelsTest.java
@@ -5,10 +5,10 @@ import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.*;
-
 
 /**
  *
@@ -21,6 +21,7 @@ public class ModelsTest extends BaseTest {
 
     @Before
     public void setUp() {
+        populate();
     }
 
     @After
@@ -31,14 +32,50 @@ public class ModelsTest extends BaseTest {
     public void createUser() {
         // Arrange
         Map<String, String> data = new HashMap<>();
-        User testUser = new User("masteryoda", "123456");
+        User testUser = new User("r2d2", "123456");
 
         // Act
         testUser.save();
-        User expectedUser = User.fetchByUsername("masteryoda");
+        User expectedUser = User.fetchByUsername("r2d2");
 
         // Assert
         assertEquals(expectedUser.getUserName(), testUser.getUserName());
         assertEquals(expectedUser.getPassword(), testUser.getPassword());
+    }
+
+    @Test
+    public void testFetchByUsername() {
+        // Arrange
+        User expectedUser = new User(USERNAME, PASSWORD);
+
+        // Act
+        User actualUser = User.fetchByUsername(USERNAME);
+
+        // Assert
+        assertNotNull(actualUser);
+        assertEquals(expectedUser.getUserName(), actualUser.getUserName());
+        assertEquals(expectedUser.getPassword(), actualUser.getPassword());
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testFetchAll() {
+        // Arrange
+        User expectedUser = new User(USERNAME, PASSWORD);
+
+        // Act
+        List<User> actualUsers = User.fetchAllUsers();
+
+        // Assert
+        assertNotNull(actualUsers);
+        assertTrue(actualUsers.contains(expectedUser));
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testCreateItem() {
+        // Arrange
+        Sale sale = Sale.fetchAllSales().get(0);
+        Item expectedItem = new Item("Test", "Test", 100, 89.99);
+
+
     }
 }
